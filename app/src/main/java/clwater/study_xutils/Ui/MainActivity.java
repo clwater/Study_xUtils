4b3d2901aba2b90ccf234d851337922863b3b496
clwater <gengzhibo@gengzhibo.net>
2016-06-07 23:48:38 +0800

    action bar

--- app/src/main/java/clwater/study_xutils/Ui/MainActivity.java
+++ app/src/main/java/clwater/study_xutils/Ui/MainActivity.java
@@ -5,9 +5,12 @@ import android.app.ActionBar;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
@@ -40,19 +43,22 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
 
 
-        if(true){
-            
-        }
 
 
-        ActionBar actionBar = this.getActionBar();
-
-        //actionBar.show();
-        actionBar.setSubtitle("mytest");
-        actionBar.setTitle("vogella.com");
 
 
         x.view().inject(this);
+
+
+        android.support.v7.app.ActionBar actionBar = getSupportActionBar();
+        actionBar.setSubtitle("副标题");
+        actionBar.setTitle("主标题");
+        actionBar.setDisplayShowHomeEnabled(true);
+        actionBar.setLogo(R.drawable.test);
+        actionBar.setDisplayUseLogoEnabled(true);
+
+
+
 
 
 
@@ -93,6 +99,30 @@ public class MainActivity extends AppCompatActivity {
         );
         mRequestQueue.add(request);
 
+    }
+
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to theaction bar if it is present.
+        getMenuInflater().inflate(R.menu.menu, menu);//获取menu目录下simple.xml的菜单文件
+        return true;
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        switch (id) {
+            case R.id.refresh:
+                Toast.makeText(this , "您点击了刷新菜单" , Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.cancle:
+                Toast.makeText(this , "您点击了取消菜单" , Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.setting:
+                Toast.makeText(this , "您点击了设置菜单" , Toast.LENGTH_SHORT).show();
+                return true;
+
+        }
+        return super.onOptionsItemSelected(item);
     }
 
 }
