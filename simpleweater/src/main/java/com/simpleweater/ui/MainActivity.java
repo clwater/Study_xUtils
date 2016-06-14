4bbfb31b06917362295539dee780e8a08211ea8c
clwater <gengzhibo@gengzhibo.net>
2016-06-14 19:34:20 +0800

    启东时判断登录情况

--- simpleweater/src/main/java/com/simpleweater/ui/MainActivity.java
+++ simpleweater/src/main/java/com/simpleweater/ui/MainActivity.java
@@ -1,8 +1,12 @@
 package com.simpleweater.ui;
 
+import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -22,12 +26,15 @@ public class MainActivity extends AppCompatActivity {
     private TextView mian_info;
 
     public static String city = "nochoose";
+    private Activity activity;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         x.view().inject(this);
+
+        activity = this;
 
         android.support.v7.app.ActionBar actionBar = getSupportActionBar();
         //actionBar.setSubtitle("副标题");
@@ -36,9 +43,14 @@ public class MainActivity extends AppCompatActivity {
         actionBar.setLogo(R.drawable.test);
         actionBar.setDisplayUseLogoEnabled(true);
 
+
+        pd = checklocation();
+
         if (pd){
             startActivity(new Intent(this , ChooseActivityOfP.class));
             pd = false;
+        }else{
+            mian_info.setText(city);
         }
 
 
@@ -46,11 +58,27 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    private boolean checklocation() {
+
+        SharedPreferences sp = activity.getSharedPreferences("SW", MODE_PRIVATE);
+        city =  sp.getString("location", "nochoose");
+
+        if(city.equals("nochoose")){
+            return true;
+        }else {
+            return false;
+        }
+    }
+
     @Override
     protected void onResume() {
         super.onResume();
         if (pd2) {
             mian_info.setText(city);
+            SharedPreferences sp = activity.getSharedPreferences("SW", MODE_PRIVATE);
+            SharedPreferences.Editor editor = sp.edit();
+            editor.putString("location", city);
+            editor.commit();
         }
     }
 }
